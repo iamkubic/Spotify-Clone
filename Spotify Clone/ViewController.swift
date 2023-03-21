31b00698e8a315f93518a304167b6e2eb9f94510
Vig <58289370+iamkubic@users.noreply.github.com>
2023-03-21 10:01:35 +0530

    Initial Commit

---
+++ Spotify Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Spotify Clone
+//
+//  Created by Vighnesh Pradhan on 21/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
